81f0a6029e5f4122ec266acc95a4dabd39eb3c74
Mansur Ziiatdinov <gltronred@gmail.com>
2023-07-08 09:12:18 +0300

    Script to submit solution

---
+++ solutions/upload1.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ -z "$TOKEN" ]; then
+    echo "Please set env var TOKEN"
+    exit 1
+fi
+
+prob=$1
+sol_file=$2
+
+curl -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" https://api.icfpcontest.com/submission --data "{\"problem_id\": $prob, \"contents\": $(jq '. | tostring' $sol_file)}"
