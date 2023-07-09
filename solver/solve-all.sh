e3f3c742188ba3728e3f4ac9085e26a87f3cd362
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2023-07-09 11:29:03 +0300

    Submitted tasks 28, 39, 40, 41, 42, 43, 44, 45, 50, 51, 52, 53, 55

--- solver/solve-all.sh
+++ solver/solve-all.sh
@@ -5,5 +5,5 @@ for num in $(cat ../problems/stats.txt | grep -oE '^[0-9]+'); do
   echo "Solving $num"
   mkdir -p ../logs
   rm -f ../logs/$num.log
-  time cargo -q run --release -- problem -i ../problems/$num.json -o ../solutions/$num.json -l ../logs/$num.log --rand-iters 1000000 --rand-max-secs 60
+  time cargo -q run --release -- problem -i ../problems/$num.json -o ../solutions/$num.json -l ../logs/$num.log --rand-iters 1000000 --rand-max-secs 600
 done;
